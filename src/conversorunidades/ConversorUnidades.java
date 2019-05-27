0b4d08331d8bd724fd9708e77f51145d496bb499
Oscar <oscarulloeldeloscamiones@gmail.com>
2019-05-28 00:02:45 +0200

    Cambio Main

--- src/conversorunidades/ConversorUnidades.java
+++ src/conversorunidades/ConversorUnidades.java
@@ -5,17 +5,73 @@
  */
 package conversorunidades;
 
+import java.util.Scanner;
+
 /**
  *
  * @author thete
  */
-public class ConversorUnidades {
+public class ConversorUnidades implements Operaciones{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /* Menu de inicio para que el usuario elija la accion a realizar*/
+        int op;
+        op = MostrarMenuPrincipal();
+        switch (op) {
+            /* Longitudes*/
+            case 0:
+                break;
+            case 1:
+                menuLongitud();
+                break;
+                /*Potencia*/
+           // case 2:menuPotencia();
+            //    break;
+
+        }
+
+    }//Fin de main
+    /*Muestra el menu principal*/
+    public static int MostrarMenuPrincipal() {
+        Scanner e = new Scanner(System.in);
+        int op;
+        System.out.println("Elija una opción");
+        System.out.println("1.Longitud");
+        System.out.println("2.Potencia");
+
+        op = Integer.valueOf(e.nextLine());
+
+        return op;
+    }
+    /* Muestra el menu para las operaciones con longitudes */
+    public static void menuLongitud(){
+        Scanner e = new Scanner(System.in);
+        int op;
+        System.out.println("Elija una opción");
+        System.out.println("1.Transformar de Km a Millas");
+        System.out.println("2.Transformar de Millas a Km");
+        System.out.println("3.Transformar de Millas a Pulgadas");
+        System.out.println("4.Transformar de Pulgadas a Millas");
+
+        op = Integer.valueOf(e.nextLine());
+
+       
+        switch(op){
+            case 0 : MostrarMenuPrincipal();
+            break;
+            case 1: kmsAMillas();
+            break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
+
     }
     
 }
